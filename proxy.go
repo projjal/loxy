4c4f81853aab7a22710b4a5b00a5aa38b79e8684
projjal <iam@pchanda.com>
2019-07-15 03:49:13 +0530

    Added https hanlder

--- proxy.go
+++ proxy.go
@@ -3,8 +3,10 @@ package main
 import (
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // HTTPProxy implements http.Handler interface
@@ -14,26 +16,66 @@ type HTTPProxy struct {
 }
 
 func (proxy *HTTPProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	resp, err := proxy.transport.RoundTrip(r)
-	if err != nil {
-		proxy.logger.Printf("error received: %s", err.Error())
+	if r.Method == "CONNECT" {
+		proxy.handleHTTPS(w, r)
+	} else {
+		resp, err := proxy.transport.RoundTrip(r)
+		if err != nil {
+			proxy.logger.Printf("error received: %s", err.Error())
+			return
+		}
+
+		header := w.Header()
+		for k, v := range resp.Header {
+			for _, val := range v {
+				header.Add(k, val)
+			}
+		}
+
+		_, err = io.Copy(w, resp.Body)
+
+		if err = resp.Body.Close(); err != nil {
+			proxy.logger.Printf("error closing response body")
+		}
+		proxy.logger.Printf("success")
+	}
+}
+
+func (proxy *HTTPProxy) handleHTTPS(w http.ResponseWriter, r *http.Request) {
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		proxy.logger.Printf("doesn't supprt hijacking")
 		return
 	}
+	hjConn, _, err := hj.Hijack()
+	if err != nil {
+		proxy.logger.Printf("error hijacking connection: %s", err.Error())
+		return
+	}
+	defer hjConn.Close()
 
-	header := w.Header()
-	for k, v := range resp.Header {
-		for _, val := range v {
-			header.Add(k, val)
-		}
+	host := r.URL.Host
+	if !strings.Contains(host, ":") {
+		host += ":80"
 	}
 
-	_, err = io.Copy(w, resp.Body)
+	proxy.logger.Printf("Accepting CONNECT request")
+	hjConn.Write([]byte("HTTP/1.0 200 OK\r\n\r\n"))
 
-	if err = resp.Body.Close(); err != nil {
-		proxy.logger.Printf("error closing response body")
+	targetConn, err := net.Dial("tcp", host)
+	if err != nil {
+		proxy.logger.Printf("error connecting to target after CONNECT: %s", err.Error())
+		return
 	}
-	proxy.logger.Printf("success")
+	defer targetConn.Close()
 
+	go func() {
+		io.Copy(targetConn, hjConn)
+	}()
+	// go func() {
+	// 	io.Copy(hjConn, targetConn)
+	// }()
+	io.Copy(hjConn, targetConn)
 }
 
 // NewHTTPProxy returns a new HTTPProxy
